db6d825a100b49d50c84233ed92b5d1039e7bf12
Ghanavishmathi-Macharla <munnymacharla91@gmail.com>
2022-06-10 15:40:12 +0530

    updated main.go

--- Basic-API/main.go
+++ Basic-API/main.go
@@ -6,6 +6,12 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+type Book struct {
+	ID     string `json:"id"`
+	Title  string `json:"title"`
+	Author string `json:"author"`
+}
+
 func main() {
 	r := gin.New()
 	r.GET("/", func(c *gin.Context) {
